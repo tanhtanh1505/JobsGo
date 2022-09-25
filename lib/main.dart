ebaa368dd03a907baf958e259993cd407736afca
hieupham297 <hieupham290702@gmail.com>
2022-09-25 22:41:59 +0700

    Welcome Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jobsgo/screens/WelcomePages/WelcomePage.dart';
 import 'screens/Home.dart';
 import 'screens/SavedJobs.dart';
 import 'screens/Application.dart';
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         fontFamily: 'Poppins',
       ),
-      home: const MyHomePage(title: 'Flutter demo'),
+      home: const WelcomePage(),
     );
   }
 }
---
+++ lib/screens/WelcomePages/WelcomePage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:jobsgo/screens/WelcomePages/Body.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
